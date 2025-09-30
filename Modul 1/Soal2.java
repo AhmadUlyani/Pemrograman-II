9a2f2b5a13724471541570ce61891d27f7f7462c
AhmadUlyani <jolam.fc382@gmail.com>
2025-09-30 21:53:12 +0800

    Add files via upload

---
+++ Modul 1/Soal2.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Soal2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan input: ");
+        int start = input.nextInt();
+
+        int count = 0;
+        int n = start;
+
+        while (count < 11) {
+            int output;
+            if (n % 5 == 0) {
+                output = (n / 5) - 1;
+            } else {
+                output = n;
+            }
+            System.out.print(output);
+            if (count < 10) {
+                System.out.print(", ");
+            }
+            n++;
+            count++;
+        }
+
+        input.close();
+    }
+}
---
+++ Modul 1/Soal4.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Soal4 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Tangan Abu: ");
+        String[] abuHands = input.nextLine().split(" ");
+
+        System.out.print("Tangan Bagas: ");
+        String[] bagasHands = input.nextLine().split(" ");
+
+        int skorAbu = 0;
+        int skorBagas = 0;
+
+        for (int i = 0; i < 3; i++){
+            String abuChoose = abuHands[i];
+            String bagasChoose = bagasHands[i];
+
+            if ((abuChoose.equals("B") && bagasChoose.equals("G")) ||
+                    (abuChoose.equals("G") && bagasChoose.equals("K")) ||
+                    (abuChoose.equals("K") && bagasChoose.equals("B"))) {
+                skorAbu++;
+            }
+            else if ((bagasChoose.equals("B") && abuChoose.equals("G")) ||
+                    (bagasChoose.equals("G") && abuChoose.equals("K")) ||
+                    (bagasChoose.equals("K") && abuChoose.equals("B"))) {
+                skorBagas++;
+            }
+        }
+
+        if (skorAbu > skorBagas) {
+            System.out.print("\nAbu");
+        } else if (skorBagas > skorAbu) {
+            System.out.print("\nBagas");
+        } else {
+            System.out.print("\nSeri");
+        }
+
+        input.close();
+    }
+}
