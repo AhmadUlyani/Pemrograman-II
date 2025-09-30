7f2f72feee6fc8779f5b5744a9fa7b688ac230c1
AhmadUlyani <jolam.fc382@gmail.com>
2025-09-30 23:09:27 +0800

    Add files via upload

---
+++ Modul1/PRAK102_2410817210008_AhmadUlyani.java
@@ -0,0 +1,31 @@
+package Modul1;
+import java.util.Scanner;
+
+public class PRAK102_2410817210008_AhmadUlyani {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan input: ");
+        int start = input.nextInt();
+
+        int count = 0;
+        int n = start;
+
+        while (count < 11) {
+            int output;
+            if (n % 5 == 0) {
+                output = (n / 5) - 1;
+            } else {
+                output = n;
+            }
+            System.out.print(output);
+            if (count < 10) {
+                System.out.print(", ");
+            }
+            n++;
+            count++;
+        }
+
+        input.close();
+    }
+}
