f662a77c34614e9dcc7edad30b109e45005c66ef
AhmadUlyani <jolam.fc382@gmail.com>
2025-11-05 17:35:21 +0800

    Add files via upload

---
+++ Praktikum5/Soal1/PaintThings.java
@@ -0,0 +1,26 @@
+package Praktikum5.Soal1;
+import java.util.Locale;
+
+public class PaintThings {
+    public static void main(String[] args) {
+        final double COVERAGE = 350;
+        Paint paint = new Paint(COVERAGE);
+        Rectangle deck;
+        Sphere bigBall;
+        Cylinder tank;
+
+        double deckAmt, ballAmt, tankAmt;
+        deck = new Rectangle(20.0, 30.0);
+        bigBall = new Sphere(15.0);
+        tank = new Cylinder(10.0, 30.0);
+
+        deckAmt = paint.amount(deck);
+        ballAmt = paint.amount(bigBall);
+        tankAmt = paint.amount(tank);
+
+        System.out.println("\nNumber of gallons of paint needed...");
+        System.out.printf(Locale.US,"Deck %.1f%n", deckAmt);
+        System.out.printf(Locale.US,"Big Ball %.1f%n", ballAmt);
+        System.out.printf(Locale.US, "Tank %.1f%n", tankAmt);
+    }
+}
