a588bdd58b6a4287507c36874b5e316e632575d4
AhmadUlyani <jolam.fc382@gmail.com>
2025-11-05 17:31:38 +0800

    Add files via upload

---
+++ Praktikum5/Shape.java
@@ -0,0 +1,14 @@
+package Praktikum5;
+
+public abstract class Shape {
+    protected String shapeName;
+    public Shape(String name){
+        this.shapeName = name;
+    }
+
+    public abstract Double area();
+
+    public String toString(){
+        return this.shapeName;
+    }
+}
